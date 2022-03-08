e1fe055340684750c446e49894912175502f17ef
arumbaua366 <arumbaua366@gmail.com>
2022-03-08 01:34:13 -0800

    added additional components nav and footer, updated about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,21 @@
 import React from "react";
 
 export default function About() {
-  <div>
-    <h1>Hello. My name is Andrea Rumbaua.</h1>
-    <p>
-      I'm a Developer based in the San Francisco Bay Area. I am a recent
-      graduate of the UC Davis Full-Stack Web Development program. Although my
-      9+ years of professional experience is in finance and human resources,
-      those industries introduced me to my love for code. I'm a passionate
-      problem solver and aim to create technology that can help solve the
-      smallest problems. I also love musicals, cooking, travel gear 🧳 (as well
-      as traveling), and sci-fi movies/shows.
-    </p>
-  </div>;
+  return (
+    <section>
+      <div>
+        <h1>Hello. My name is Andrea Rumbaua.</h1>
+        <p>
+          I'm a Developer based in the San Francisco Bay Area. I am a recent
+          graduate of the UC Davis Full-Stack Web Development program. Although
+          my 9+ years of professional experience is in finance and human
+          resources, those industries introduced me to my love for code. I'm a
+          passionate problem solver and aim to create technology that can help
+          solve the smallest problems. I also love musicals, cooking, travel
+          gear 🧳 (as well as traveling), and sci-fi movies/shows.
+        </p>
+      </div>
+      ;
+    </section>
+  );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer () {
+    return (
+        <footer>
+            <div>
+                <div>
+                    This is supposed to be the footer
+                </div>
+            </div>
+        </footer>
+    )
+}

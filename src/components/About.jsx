4d435fc8298fb19581f082dadd5bbccc6df65ac7
arumbaua366 <arumbaua366@gmail.com>
2022-03-20 04:05:05 -0700

    fixed font size on about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,18 +4,18 @@ export default function About() {
   return (
     <>
       <div id="about" className="section scrollspy h-100 p-5 m-5">
-        <div className="container-fluid bg-light card mb-4">
+        <div className="container-fluid d-flex bg-light card mb-4">
           <div className="row-md-6 row-xs-12">
             <div className="card-body">
-              <h1 className="title text-center display-5 p-4 m-5">about.</h1>
-              <p className="lead fs-3 fw-light m-5">
+            <h1 className="title text-center display-5 text-black pt-5 mb-4">about.</h1>
+              <p className="lead fw-light m-5">
                 Self-motivated, voluntary learner devoted to the lifelong
                 pursuit of knowledge. Although my 8+ years of professional
                 experience is in other industries such as health, finance, and
                 human resources, those industries introduced me to my love for
                 code.
               </p>
-              <p className="lead fs-3 fw-light m-5">
+              <p className="lead fw-light m-5">
                 Committed to team dynamics and employing determination,
                 patience, and persistence to solve even the smallest problems.
               </p>
